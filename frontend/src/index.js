438a8cf521e67eff828ba16968255e6caa6e52f2
Krushna Bade <krushnabade54@gmail.com>
2026-09-18 04:04:12 +0530

    Apply GOLD-e AI growth UI layer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import "./dark.css";
 import "./gold-eai-system.css";
 import "./app-overrides.css";
+import "./growth-overrides.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
