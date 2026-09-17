ec34609b5d3add42ac0243648366d10e74441011
Krushna Bade <krushnabade54@gmail.com>
2026-09-17 11:12:32 +0530

    Fix frontend entry point for GOLD-e AI app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./dark.css";
 import "./gold-eai-system.css";
 import "./app-overrides.css";
-import App from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import "./App";
