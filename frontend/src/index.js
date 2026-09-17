44a3993a5d94dd280b5db828bfeefb41ec4454a4
Krushna Bade <krushnabade54@gmail.com>
2026-09-17 11:12:52 +0530

    Restore React entry rendering after App export fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./dark.css";
 import "./gold-eai-system.css";
 import "./app-overrides.css";
-import "./App";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
