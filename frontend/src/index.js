947ab7d6c4c2a7c31f7ee2620f5950a9315929da
Krushna Bade <krushnabade54@gmail.com>
2026-09-18 04:26:08 +0530

    Wire growth UI interactions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import "./dark.css";
 import "./gold-eai-system.css";
 import "./app-overrides.css";
 import "./growth-overrides.css";
+import "./growth-behavior.js";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
